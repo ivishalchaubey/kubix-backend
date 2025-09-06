2aa49e6ca7f6f63a885e5532f78ffe818bcaa205
chirag858 <arorachirag858@gmail.com>
2025-09-06 23:28:00 +0530

    update token and token details of us=niveriity

--- src/app/modules/user/controllers/user.ts
+++ src/app/modules/user/controllers/user.ts
@@ -83,7 +83,7 @@ class UserController {
             const UserId = req.user._id;
             
             const user = await this.userService.updateToken(UserId, req.body.token);
-            ResponseUtil.success(res, user, "Token updated successfully");
+            ResponseUtil.success(res, user, API_MESSAGES.USER.TOKEN_UPDATED);
         } catch (error) {
             next(error);
         }
